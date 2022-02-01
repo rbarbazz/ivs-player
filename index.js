e7a18dfb39fca6a068987f325d1e984a9de22d1f
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2022-01-31 23:34:30 -0800

    feat: add url text input

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-if (IVSPlayer.isPlayerSupported) {
-  const player = IVSPlayer.create()
+const DEFAULT_URL =
+  'https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8'
+let player
 
+if (IVSPlayer.isPlayerSupported) {
+  player = IVSPlayer.create()
   player.attachHTMLVideoElement(document.getElementById('video-player'))
-  player.load(
-    'https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8',
-  )
+
+  player.load(DEFAULT_URL)
   player.play()
+}
+
+function onBtnClick(elem) {
+  if (!player) {
+    return
+  }
+
+  const val = document.getElementById('feed-url').value
+
+  if (val !== '') {
+    player.load(val)
+    player.play()
+  }
 }
